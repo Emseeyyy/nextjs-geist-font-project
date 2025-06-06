43d6c6cdf6d5191f47637732d0af6eab6e8fe487
Emseeyyy <mca.odesk@gmail.com>
2025-06-06 19:08:30 +1000

    Add src/app/tradespeople/components/JobCompletion.tsx 1749200910288

---
+++ src/app/tradespeople/components/JobCompletion.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+
+export default function JobCompletion() {
+  return (
+    <section className="p-4 border rounded-md mb-4">
+      <h3 className="text-2xl font-semibold mb-2">Mark Jobs as Completed & Send Invoices</h3>
+      <p>Feature to mark jobs as completed and send invoices will be implemented here.</p>
+    </section>
+  );
+}
