14c5c60d2df6057efe3908931db2316b08990576
Emseeyyy <mca.odesk@gmail.com>
2025-06-06 19:08:41 +1000

    Add src/app/tradespeople/components/ProfileManagement.tsx 1749200921453

---
+++ src/app/tradespeople/components/ProfileManagement.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+
+export default function ProfileManagement() {
+  return (
+    <section className="p-4 border rounded-md mb-4">
+      <h3 className="text-2xl font-semibold mb-2">Create & Manage Profile</h3>
+      <p>Feature to create and manage profile (name, ABN, licenses, availability, service types) will be implemented here.</p>
+    </section>
+  );
+}
