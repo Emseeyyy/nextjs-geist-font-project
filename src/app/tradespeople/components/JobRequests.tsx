e44cc6a3170281570c13ba627601113f4432ed23
Emseeyyy <mca.odesk@gmail.com>
2025-06-06 19:08:40 +1000

    Add src/app/tradespeople/components/JobRequests.tsx 1749200920222

---
+++ src/app/tradespeople/components/JobRequests.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+
+export default function JobRequests() {
+  return (
+    <section className="p-4 border rounded-md mb-4">
+      <h3 className="text-2xl font-semibold mb-2">Receive & Accept Job Requests</h3>
+      <p>Feature to receive and accept job requests will be implemented here.</p>
+    </section>
+  );
+}
