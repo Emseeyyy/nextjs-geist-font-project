2b4a5540ad3465ed473f6ea47ce1c028042968bf
Emseeyyy <mca.odesk@gmail.com>
2025-06-06 19:08:31 +1000

    Add src/app/tradespeople/components/Chat.tsx 1749200911619

---
+++ src/app/tradespeople/components/Chat.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+
+export default function Chat() {
+  return (
+    <section className="p-4 border rounded-md mb-4">
+      <h3 className="text-2xl font-semibold mb-2">Chat with Clients</h3>
+      <p>Feature to chat with clients will be implemented here.</p>
+    </section>
+  );
+}
