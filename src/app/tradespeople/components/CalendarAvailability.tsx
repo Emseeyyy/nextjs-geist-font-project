d4ac18640e91c58b08afbbff23f591baf5dd821f
Emseeyyy <mca.odesk@gmail.com>
2025-06-06 19:08:38 +1000

    Add src/app/tradespeople/components/CalendarAvailability.tsx 1749200918766

---
+++ src/app/tradespeople/components/CalendarAvailability.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+
+export default function CalendarAvailability() {
+  return (
+    <section className="p-4 border rounded-md mb-4">
+      <h3 className="text-2xl font-semibold mb-2">Set Calendar Availability</h3>
+      <p>Feature to set calendar availability will be implemented here.</p>
+    </section>
+  );
+}
